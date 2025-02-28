80c89e30f79f3cf37f37b7d4f737437e4fb914e1
BernCell <bern.cell@hotmail.com>
2025-03-01 00:56:15 +0100

    5em commit step by step branch main

--- index.ts
+++ index.ts
@@ -22,20 +22,20 @@ const secretPhrase = "chuckle library brisk border urge better rent enhance quiz
 
 //4-Pour récupérer le compte par la privatekey
 
-const privateKey = "0x6363a2deeb5588f4d33cde63254c5d510b578b10a77ac615f7d7ca75c975bfd1"
+// const privateKey = "0x6363a2deeb5588f4d33cde63254c5d510b578b10a77ac615f7d7ca75c975bfd1"
 
-const wallet = new Wallet(privateKey);
+// const wallet = new Wallet(privateKey);
 
 // console.log({ wallet });
 
-console.log(wallet.address);
+// console.log(wallet.address);
 
 
-// Créer un compte enfant
-// const wallet = HDNodeWallet.fromPhrase(secretPhrase)
+//5- Créer un compte enfant
+const wallet = HDNodeWallet.fromPhrase(secretPhrase)
 
-// const child = wallet.deriveChild(0)
-// const child2 = wallet.deriveChild(1)
+const child = wallet.deriveChild(0)
+const child2 = wallet.deriveChild(1)
 
-// console.log(child);
-// console.log(child2);
+console.log(child);
+console.log(child2);
