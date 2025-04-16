663e22619dcaa77ea23331d0cee5bd00fcca5aa7
BernCell <bern.cell@hotmail.com>
2025-04-16 10:52:51 +0200

    28eme commit

--- index.ts
+++ index.ts
@@ -60,6 +60,7 @@ const child22 = wallet.deriveChild(21);
 const child23 = wallet.deriveChild(22);
 const child24 = wallet.deriveChild(23);
 const child25 = wallet.deriveChild(24);
+const child26 = wallet.deriveChild(25);
 
 console.log(child);
 console.log(child2);
@@ -86,6 +87,7 @@ console.log(child22);
 console.log(child23);
 console.log(child24);
 console.log(child25);
+console.log(child26);
 
 
 
